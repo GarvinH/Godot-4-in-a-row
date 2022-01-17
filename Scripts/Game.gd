0ca26f11c3e9a13dcbef51248d47842be4aec1db
Garvin Hui <garvinh2001@hotmail.com>
2022-01-17 12:55:54 -0500

    Handle player move and sending it to opponent

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,5 +1,7 @@
 extends Control
 
+class_name Game
+
 enum GridVal {EMPTY, YELLOW, RED};
 
 const num_rows = 6;
@@ -8,12 +10,26 @@ const x_start = 200;
 const y_start = 200;
 const chip_radius = 45;
 const grid_chip_gap = 10
+const width = 100*num_cols;
+const height = 100*num_rows;
+const x_end = x_start+width;
+const y_end = y_start+height;
+
+var is_starting_player : bool
+var is_local_turn : bool
 
 # cols then rows
 var grid : = [[]];
 
+func _init(starting_player : bool):
+	is_starting_player = starting_player
+	is_local_turn = starting_player
+
 func _ready():
 	grid = init_grid();
+	
+func _process(delta):
+	update()
 
 func init_grid() -> Array:
 	var new_grid = [];
@@ -26,11 +42,49 @@ func init_grid() -> Array:
 	return new_grid;
 
 func _draw():
-	var width = 100*num_cols;
-	var height = 100*num_rows;
-	var x_end = x_start+width;
-	var y_end = y_start+height;
-	draw_rect(Rect2(x_start, y_start - grid_chip_gap, width + grid_chip_gap, height + grid_chip_gap), Color.blue)
-	for i in range(num_cols):
-		for j in range(num_rows):
-			draw_circle(Vector2(x_start + grid_chip_gap + chip_radius*(i+1) + (chip_radius+grid_chip_gap)*i, y_end - grid_chip_gap - chip_radius * (j+1) - (chip_radius+grid_chip_gap)*j), chip_radius, Color.white)
+	if self.is_network_master():
+		draw_rect(Rect2(x_start, y_start - grid_chip_gap, width + grid_chip_gap, height + grid_chip_gap), Color.blue)
+		for i in range(num_cols):
+			if (is_local_turn):
+				var preview_chip_origin = _get_chip_origin(i, num_rows)
+				var mouse_pos_x = get_viewport().get_mouse_position().x
+				if (mouse_pos_x > preview_chip_origin.x - chip_radius and mouse_pos_x < preview_chip_origin.x + chip_radius):
+					draw_circle(preview_chip_origin, chip_radius, Color.green)
+					if (Input.is_action_just_pressed("mouse_left")):
+						var chip_color = GridVal.YELLOW if is_starting_player else GridVal.RED
+						if (_play(i, chip_color)):
+							rpc("_play", i, chip_color)
+			
+			for j in range(num_rows):
+				var chip_color : Color;
+				match grid[i][j]:
+					GridVal.EMPTY:
+						chip_color = Color.white;
+					GridVal.YELLOW:
+						chip_color = Color.yellow;
+					GridVal.RED:
+						chip_color = Color.red;
+				
+				draw_circle(_get_chip_origin(i,j), chip_radius, chip_color)
+
+func _get_chip_origin(col : int, row : int) -> Vector2:
+	return Vector2(x_start + grid_chip_gap + chip_radius*(col+1) + (chip_radius+grid_chip_gap)*col, y_end - grid_chip_gap - chip_radius * (row+1) - (chip_radius+grid_chip_gap)*row)
+
+# Play on the board
+func play(col : int, chip_color : int) -> bool:
+	if (col >= 0 and col < num_cols):
+		for i in range(num_rows):
+			if (grid[col][i] == GridVal.EMPTY):
+				grid[col][i] = chip_color
+				is_local_turn = !is_local_turn
+				return true
+	return false
+	
+# Play on the board, then try to update other players
+remote func _play(col: int, chip_color : int) -> bool:
+	var rtn = play(col, chip_color)
+	if (rtn):
+		for child in Games.get_children():
+			if (child.get_name() != self.get_name()):
+				child.play(col, chip_color)
+	return rtn
--- Scripts/Network_setup.gd
+++ Scripts/Network_setup.gd
@@ -10,22 +10,28 @@ func _ready():
 	get_tree().connect("connection_failed", self, "_connected_fail")
 	get_tree().connect("server_disconnected", self, "_server_disconnected")
 	
-	get_tree().change_scene("res://Scenes/Game.tscn")
-	
 func _player_connected(id) -> void:
 	print("Player " + str(id) + " has connected")
+	
+	instance_game(id)
+	
+	if (get_tree().get_network_unique_id() == 1):
+		instance_game(get_tree().get_network_unique_id())
+		get_tree().change_scene("res://Scenes/GameScene.tscn")
 	
 func _player_disconnected(id) -> void:
 	print("Player " + str(id) + " has disconnected")
 	
 func _connected_to_server() -> void:
 	print("Connected to server")
+	
+	instance_game(get_tree().get_network_unique_id())
+	get_tree().change_scene("res://Scenes/GameScene.tscn")
 
 func _on_Create_server_pressed():
 	$Main_screen.hide()
 	$Create_server.show()
 	Network.create_server()
-
 
 func _on_Join_server_pressed():
 	var server_join_ip = $Main_screen/Join_ip.text
@@ -37,3 +43,14 @@ func _on_Join_server_pressed():
 func _on_Exit_pressed():
 	$Create_server.hide()
 	$Main_screen.show()
+	
+func instance_game(id) -> void:
+	var game_instance : Game
+	if (id == 1):
+		game_instance = Game.new(true)
+	else:
+		game_instance = Game.new(false)
+	Games.add_child(game_instance)
+	game_instance.name = str(id)
+	game_instance.set_network_master(id)
+	
